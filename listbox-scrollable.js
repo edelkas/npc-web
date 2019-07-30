763d4b6b8d7821a7e77826a80015948e59b39565
edelkas <edlucasma@gmail.com>
2019-07-30 02:47:20 +0200

    fixed clicking on listbox items

--- listbox-scrollable.js
+++ listbox-scrollable.js
@@ -9,5 +9,10 @@
  */
 
 window.addEventListener('load', function () {
-  var exListbox = new aria.Listbox(document.getElementById('ss_elem_list'));
+  var listbox_objects = new aria.Listbox(document.getElementById('list_objects'));
+  var listbox_menu = new aria.Listbox(document.getElementById('list_menu'));
+  var listbox_editor = new aria.Listbox(document.getElementById('list_editor'));
+  var listbox_timebar = new aria.Listbox(document.getElementById('list_timebar'));
+  var listbox_headbands = new aria.Listbox(document.getElementById('list_headbands'));
+  var listbox_effects = new aria.Listbox(document.getElementById('list_effects'));
 });
