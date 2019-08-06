9cd00b50322aec8f369ebc2daca90cec7300320d
edelkas <edlucasma@gmail.com>
2019-08-06 23:18:11 +0200

    code refactor

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
-function add_buttons(i, o, indexes, sect){
+var buttons = [
+	{name:"background", section:"Objects_Items", indexes:[]},
+	{name:"ninja", section:"Objects_Items", indexes:[]},
+	{name:"entityMine", section:"Objects_Items", indexes:[]},
+	{name:"entityGold", section:"Objects_Items", indexes:[]},
+	{name:"entityDoorExit", section:"Objects_Items", indexes:[]},
+	{name:"entityDoorExitSwitch", section:"Objects_Items", indexes:[] },
+	{name:"entityDoorRegular", section:"Objects_Items", indexes:[] },
+	{name:"entityDoorLocked", section:"Objects_Items", indexes:[] },
+	{name:"entityDoorTrap", section:"Objects_Items", indexes:[] },
+	{name:"entityLaunchPad", section:"Objects_Items", indexes:[] },
+	{name:"entityOneWayPlatform", section:"Objects_Items", indexes:[] },
+	{name:"entityDroneChaingun", section:"Objects_Items", indexes:[] },
+	{name:"entityDroneLaser", section:"Objects_Items", indexes:[] },
+	{name:"entityDroneZap", section:"Objects_Items", indexes:[] },
+	{name:"entityDroneChaser", section:"Objects_Items", indexes:[] },
+	{name:"entityFloorGuard", section:"Objects_Items", indexes:[] },
+	{name:"entityBounceBlock", section:"Objects_Items", indexes:[] },
+	{name:"entityRocket", section:"Objects_Items", indexes:[] },
+	{name:"entityTurret", section:"Objects_Items", indexes:[] },
+	{name:"entityThwomp", section:"Objects_Items", indexes:[] },
+	{name:"entityEvilNinja", section:"Objects_Items", indexes:[] },
+	{name:"entityDualLaser", section:"Objects_Items", indexes:[] },
+	{name:"entityBoostPad", section:"Objects_Items", indexes:[] },
+	{name:"entityBat", section:"Objects_Items", indexes:[] },
+	{name:"entityEyeBat", section:"Objects_Items", indexes:[] },
+	{name:"entityShoveThwomp", section:"Objects_Items", indexes:[] },
+	{name:"menu", section:"Menu_Items", indexes:[0, 3, 4, 10, 17, 29]},
+	{name:"menu", section:"Menu_Items", indexes:[8, 12, 25, 26, 27, 31, 33, 37]},
+	{name:"menu", section:"Menu_Items", indexes:[1, 2, 13, 16, 32]},
+	{name:"menu", section:"Menu_Items", indexes:[5, 7, 9, 22, 23, 30]},
+	{name:"menu", section:"Menu_Items", indexes:[18, 19, 20, 21]},
+	{name:"menu", section:"Menu_Items", indexes:[14, 15]},
+	{name:"menu", section:"Menu_Items", indexes:[24, 28, 34, 35, 38, 40, 41]},
+	{name:"menu", section:"Menu_Items", indexes:[6, 11, 36, 39]},
+	{name:"editor", section:"Editor_Items", indexes:[0, 2, 5]},
+	{name:"editor", section:"Editor_Items", indexes:[3, 6, 7, 8]},
+	{name:"editor", section:"Editor_Items", indexes:[1, 4, 9]},
+	{name:"timeBar", section:"Timebar_Items", indexes:[0, 2, 4, 6]},
+	{name:"timeBar", section:"Timebar_Items", indexes:[1]},
+	{name:"timeBarRace", section:"Timebar_Items", indexes:[1, 5, 6, 7]},
+	{name:"timeBarRace", section:"Timebar_Items", indexes:[2, 8, 9, 10]},
+	{name:"timeBarRace", section:"Timebar_Items", indexes:[3, 11, 12, 13]},
+	{name:"timeBarRace", section:"Timebar_Items", indexes:[4, 14, 15, 16]},
+	{name:"timeBar", section:"Timebar_Items", indexes:[3, 5, 7]},
+	{name:"headbands", section:"Headbands_Items", indexes:[0, 2, 4, 6]},
+	{name:"headbands", section:"Headbands_Items", indexes:[1, 3, 5, 7]},
+	{name:"headbands", section:"Headbands_Items", indexes:[8, 9, 10, 11]},
+	{name:"headbands", section:"Headbands_Items", indexes:[12, 13, 14, 15, 16]},
+	{name:"explosions", section:"Effects_Items", indexes:[]},
+	{name:"fxDroneZap", section:"Effects_Items", indexes:[]},
+	{name:"fxFloorguardZap", section:"Effects_Items", indexes:[]},
+	{name:"fxNinja", section:"Effects_Items", indexes:[]}
+]
+
+function create_button(i, o, indexes, sect){
 	var section = document.getElementById(sect);
 	var colors = getObjects(o, indexes);
 	var area = document.createElement("div");
@@ -18,62 +73,18 @@ function add_buttons(i, o, indexes, sect){
 }
 
 function create_buttons(){
-	var objs = [
-		{name:"background", section:"Objects_Items", indexes:[]},
-		{name:"ninja", section:"Objects_Items", indexes:[]},
-		{name:"entityMine", section:"Objects_Items", indexes:[]},
-		{name:"entityGold", section:"Objects_Items", indexes:[]},
-		{name:"entityDoorExit", section:"Objects_Items", indexes:[]},
-		{name:"entityDoorExitSwitch", section:"Objects_Items", indexes:[] },
-		{name:"entityDoorRegular", section:"Objects_Items", indexes:[] },
-		{name:"entityDoorLocked", section:"Objects_Items", indexes:[] },
-		{name:"entityDoorTrap", section:"Objects_Items", indexes:[] },
-		{name:"entityLaunchPad", section:"Objects_Items", indexes:[] },
-		{name:"entityOneWayPlatform", section:"Objects_Items", indexes:[] },
-		{name:"entityDroneChaingun", section:"Objects_Items", indexes:[] },
-		{name:"entityDroneLaser", section:"Objects_Items", indexes:[] },
-		{name:"entityDroneZap", section:"Objects_Items", indexes:[] },
-		{name:"entityDroneChaser", section:"Objects_Items", indexes:[] },
-		{name:"entityFloorGuard", section:"Objects_Items", indexes:[] },
-		{name:"entityBounceBlock", section:"Objects_Items", indexes:[] },
-		{name:"entityRocket", section:"Objects_Items", indexes:[] },
-		{name:"entityTurret", section:"Objects_Items", indexes:[] },
-		{name:"entityThwomp", section:"Objects_Items", indexes:[] },
-		{name:"entityEvilNinja", section:"Objects_Items", indexes:[] },
-		{name:"entityDualLaser", section:"Objects_Items", indexes:[] },
-		{name:"entityBoostPad", section:"Objects_Items", indexes:[] },
-		{name:"entityBat", section:"Objects_Items", indexes:[] },
-	  {name:"entityEyeBat", section:"Objects_Items", indexes:[] },
-		{name:"entityShoveThwomp", section:"Objects_Items", indexes:[] },
-		{name:"menu", section:"Menu_Items", indexes:[0, 3, 4, 10, 17, 29]},
-		{name:"menu", section:"Menu_Items", indexes:[8, 12, 25, 26, 27, 31, 33, 37]},
-		{name:"menu", section:"Menu_Items", indexes:[1, 2, 13, 16, 32]},
-		{name:"menu", section:"Menu_Items", indexes:[5, 7, 9, 22, 23, 30]},
-		{name:"menu", section:"Menu_Items", indexes:[18, 19, 20, 21]},
-		{name:"menu", section:"Menu_Items", indexes:[14, 15]},
-		{name:"menu", section:"Menu_Items", indexes:[24, 28, 34, 35, 38, 40, 41]},
-		{name:"menu", section:"Menu_Items", indexes:[6, 11, 36, 39]},
-		{name:"editor", section:"Editor_Items", indexes:[0, 2, 5]},
-		{name:"editor", section:"Editor_Items", indexes:[3, 6, 7, 8]},
-		{name:"editor", section:"Editor_Items", indexes:[1, 4, 9]},
-		{name:"timeBar", section:"Timebar_Items", indexes:[0, 2, 4, 6]},
-		{name:"timeBar", section:"Timebar_Items", indexes:[1]},
-		{name:"timeBarRace", section:"Timebar_Items", indexes:[1, 5, 6, 7]},
-		{name:"timeBarRace", section:"Timebar_Items", indexes:[2, 8, 9, 10]},
-		{name:"timeBarRace", section:"Timebar_Items", indexes:[3, 11, 12, 13]},
-		{name:"timeBarRace", section:"Timebar_Items", indexes:[4, 14, 15, 16]},
-		{name:"timeBar", section:"Timebar_Items", indexes:[3, 5, 7]},
-		{name:"headbands", section:"Headbands_Items", indexes:[0, 2, 4, 6]},
-		{name:"headbands", section:"Headbands_Items", indexes:[1, 3, 5, 7]},
-		{name:"headbands", section:"Headbands_Items", indexes:[8, 9, 10, 11]},
-		{name:"headbands", section:"Headbands_Items", indexes:[12, 13, 14, 15, 16]},
-		{name:"explosions", section:"Effects_Items", indexes:[]},
-		{name:"fxDroneZap", section:"Effects_Items", indexes:[]},
-		{name:"fxFloorguardZap", section:"Effects_Items", indexes:[]},
-		{name:"fxNinja", section:"Effects_Items", indexes:[]}
-	]
-	for (var i=0;i<objs.length;i++){
-		add_buttons(i, objs[i]["name"], objs[i]["indexes"], objs[i]["section"]);
+	for (var i=0;i<buttons.length;i++){
+		create_button(i, buttons[i]["name"], buttons[i]["indexes"], buttons[i]["section"]);
+	}
+}
+
+function update_button(){
+
+}
+
+function update_buttons(){
+	for (var i=0;i<buttons.length;i++){
+		update_button();
 	}
 }
 
@@ -105,6 +116,7 @@ function create_file(name){
 }
 
 function create_palette(){
+	update_buttons();
 	var objs = Object.keys(objects).map(o => blobify(create_file(o)));
 	var zip = new JSZip();
 	for (var i=0;i<objs.length;i++){ zip.file(Object.keys(objects)[i] + ".tga", objs[i]); }
