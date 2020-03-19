5e3b7fb07b32e1a37224b71935148165cb49a7b8
Zarah <znailxxor@gmail.com>
2020-03-20 00:09:12 +0100

    some bootstraps, much editor shenanegans

--- script.js
+++ script.js
@@ -6,58 +6,58 @@
 
 // Color buttons for each section of the GUI
 var buttons = [
-	{name:"background", section:"Objects_Items", indexes:[]},
-	{name:"ninja", section:"Objects_Items", indexes:[]},
-	{name:"entityMine", section:"Objects_Items", indexes:[]},
-	{name:"entityGold", section:"Objects_Items", indexes:[]},
-	{name:"entityDoorExit", section:"Objects_Items", indexes:[]},
-	{name:"entityDoorExitSwitch", section:"Objects_Items", indexes:[] },
-	{name:"entityDoorRegular", section:"Objects_Items", indexes:[] },
-	{name:"entityDoorLocked", section:"Objects_Items", indexes:[] },
-	{name:"entityDoorTrap", section:"Objects_Items", indexes:[] },
-	{name:"entityLaunchPad", section:"Objects_Items", indexes:[] },
-	{name:"entityOneWayPlatform", section:"Objects_Items", indexes:[] },
-	{name:"entityDroneChaingun", section:"Objects_Items", indexes:[] },
-	{name:"entityDroneLaser", section:"Objects_Items", indexes:[] },
-	{name:"entityDroneZap", section:"Objects_Items", indexes:[] },
-	{name:"entityDroneChaser", section:"Objects_Items", indexes:[] },
-	{name:"entityFloorGuard", section:"Objects_Items", indexes:[] },
-	{name:"entityBounceBlock", section:"Objects_Items", indexes:[] },
-	{name:"entityRocket", section:"Objects_Items", indexes:[] },
-	{name:"entityTurret", section:"Objects_Items", indexes:[] },
-	{name:"entityThwomp", section:"Objects_Items", indexes:[] },
-	{name:"entityEvilNinja", section:"Objects_Items", indexes:[] },
-	{name:"entityDualLaser", section:"Objects_Items", indexes:[] },
-	{name:"entityBoostPad", section:"Objects_Items", indexes:[] },
-	{name:"entityBat", section:"Objects_Items", indexes:[] },
-	{name:"entityEyeBat", section:"Objects_Items", indexes:[] },
-	{name:"entityShoveThwomp", section:"Objects_Items", indexes:[] },
-	{name:"menu", section:"Menu_Items", indexes:[0, 3, 4, 10, 17, 29]},
-	{name:"menu", section:"Menu_Items", indexes:[8, 12, 25, 26, 27, 31, 33, 37]},
-	{name:"menu", section:"Menu_Items", indexes:[1, 2, 13, 16, 32]},
-	{name:"menu", section:"Menu_Items", indexes:[5, 7, 9, 22, 23, 30]},
-	{name:"menu", section:"Menu_Items", indexes:[18, 19, 20, 21]},
-	{name:"menu", section:"Menu_Items", indexes:[14, 15]},
-	{name:"menu", section:"Menu_Items", indexes:[24, 28, 34, 35, 38, 40, 41]},
-	{name:"menu", section:"Menu_Items", indexes:[6, 11, 36, 39]},
-	{name:"editor", section:"Editor_Items", indexes:[0, 2, 5]},
-	{name:"editor", section:"Editor_Items", indexes:[3, 6, 7, 8]},
-	{name:"editor", section:"Editor_Items", indexes:[1, 4, 9]},
-	{name:"timeBar", section:"Timebar_Items", indexes:[0, 2, 4, 6]},
-	{name:"timeBar", section:"Timebar_Items", indexes:[1]},
-	{name:"timeBarRace", section:"Timebar_Items", indexes:[0, 1, 5, 6, 7]},
-	{name:"timeBarRace", section:"Timebar_Items", indexes:[2, 8, 9, 10]},
-	{name:"timeBarRace", section:"Timebar_Items", indexes:[3, 11, 12, 13]},
-	{name:"timeBarRace", section:"Timebar_Items", indexes:[4, 14, 15, 16]},
-	{name:"timeBar", section:"Timebar_Items", indexes:[3, 5, 7]},
-	{name:"headbands", section:"Headbands_Items", indexes:[0, 2, 4, 6]},
-	{name:"headbands", section:"Headbands_Items", indexes:[1, 3, 5, 7]},
-	{name:"headbands", section:"Headbands_Items", indexes:[8, 9, 10, 11]},
-	{name:"headbands", section:"Headbands_Items", indexes:[12, 13, 14, 15, 16]},
-	{name:"explosions", section:"Effects_Items", indexes:[]},
-	{name:"fxDroneZap", section:"Effects_Items", indexes:[]},
-	{name:"fxFloorguardZap", section:"Effects_Items", indexes:[]},
-	{name:"fxNinja", section:"Effects_Items", indexes:[]}
+    { name: "background", section: "Objects_Items", indexes: [] },
+    { name: "ninja", section: "Objects_Items", indexes: [] },
+    { name: "entityMine", section: "Objects_Items", indexes: [] },
+    { name: "entityGold", section: "Objects_Items", indexes: [] },
+    { name: "entityDoorExit", section: "Objects_Items", indexes: [] },
+    { name: "entityDoorExitSwitch", section: "Objects_Items", indexes: [] },
+    { name: "entityDoorRegular", section: "Objects_Items", indexes: [] },
+    { name: "entityDoorLocked", section: "Objects_Items", indexes: [] },
+    { name: "entityDoorTrap", section: "Objects_Items", indexes: [] },
+    { name: "entityLaunchPad", section: "Objects_Items", indexes: [] },
+    { name: "entityOneWayPlatform", section: "Objects_Items", indexes: [] },
+    { name: "entityDroneChaingun", section: "Objects_Items", indexes: [] },
+    { name: "entityDroneLaser", section: "Objects_Items", indexes: [] },
+    { name: "entityDroneZap", section: "Objects_Items", indexes: [] },
+    { name: "entityDroneChaser", section: "Objects_Items", indexes: [] },
+    { name: "entityFloorGuard", section: "Objects_Items", indexes: [] },
+    { name: "entityBounceBlock", section: "Objects_Items", indexes: [] },
+    { name: "entityRocket", section: "Objects_Items", indexes: [] },
+    { name: "entityTurret", section: "Objects_Items", indexes: [] },
+    { name: "entityThwomp", section: "Objects_Items", indexes: [] },
+    { name: "entityEvilNinja", section: "Objects_Items", indexes: [] },
+    { name: "entityDualLaser", section: "Objects_Items", indexes: [] },
+    { name: "entityBoostPad", section: "Objects_Items", indexes: [] },
+    { name: "entityBat", section: "Objects_Items", indexes: [] },
+    { name: "entityEyeBat", section: "Objects_Items", indexes: [] },
+    { name: "entityShoveThwomp", section: "Objects_Items", indexes: [] },
+    { name: "menu", section: "Menu_Items", indexes: [0, 3, 4, 10, 17, 29] },
+    { name: "menu", section: "Menu_Items", indexes: [8, 12, 25, 26, 27, 31, 33, 37] },
+    { name: "menu", section: "Menu_Items", indexes: [1, 2, 13, 16, 32] },
+    { name: "menu", section: "Menu_Items", indexes: [5, 7, 9, 22, 23, 30] },
+    { name: "menu", section: "Menu_Items", indexes: [18, 19, 20, 21] },
+    { name: "menu", section: "Menu_Items", indexes: [14, 15] },
+    { name: "menu", section: "Menu_Items", indexes: [24, 28, 34, 35, 38, 40, 41] },
+    { name: "menu", section: "Menu_Items", indexes: [6, 11, 36, 39] },
+    { name: "editor", section: "Editor_Items", indexes: [0, 2, 5] },
+    { name: "editor", section: "Editor_Items", indexes: [3, 6, 7, 8] },
+    { name: "editor", section: "Editor_Items", indexes: [1, 4, 9] },
+    { name: "timeBar", section: "Timebar_Items", indexes: [0, 2, 4, 6] },
+    { name: "timeBar", section: "Timebar_Items", indexes: [1] },
+    { name: "timeBarRace", section: "Timebar_Items", indexes: [0, 1, 5, 6, 7] },
+    { name: "timeBarRace", section: "Timebar_Items", indexes: [2, 8, 9, 10] },
+    { name: "timeBarRace", section: "Timebar_Items", indexes: [3, 11, 12, 13] },
+    { name: "timeBarRace", section: "Timebar_Items", indexes: [4, 14, 15, 16] },
+    { name: "timeBar", section: "Timebar_Items", indexes: [3, 5, 7] },
+    { name: "headbands", section: "Headbands_Items", indexes: [0, 2, 4, 6] },
+    { name: "headbands", section: "Headbands_Items", indexes: [1, 3, 5, 7] },
+    { name: "headbands", section: "Headbands_Items", indexes: [8, 9, 10, 11] },
+    { name: "headbands", section: "Headbands_Items", indexes: [12, 13, 14, 15, 16] },
+    { name: "explosions", section: "Effects_Items", indexes: [] },
+    { name: "fxDroneZap", section: "Effects_Items", indexes: [] },
+    { name: "fxFloorguardZap", section: "Effects_Items", indexes: [] },
+    { name: "fxNinja", section: "Effects_Items", indexes: [] }
 ]
 
 // File strings
@@ -65,19 +65,36 @@ var files = {};
 var files_loaded = {};
 
 // Creates the buttons of each section, on load
-function create_button(i, o, indexes, sect){
+function create_button(i, o, indexes, sect) {
 	var section = document.getElementById(sect);
 	var colors = getObjects(o, indexes);
 	var area = document.createElement("div");
 	area.id = "i" + i;
-	area.className = "section";
+    area.classList = "container section";
 	area.style.display = "none";
-	for (var j=0;j<colors.length;j++){
+    for (var j = 0; j < colors.length; j++) {
+        var row = document.createElement("div");
+        row.classList = "row";
+
+        var inputDiv = document.createElement("div");
+        inputDiv.className = "col-auto";
 		var input = document.createElement("input");
+        var inputId = o + (indexes.length == 0 ? j : indexes[j]);
+        input.id = inputId;
+        input.className = "jscolor";
+        input.value = colors[j]["color"];
+        inputDiv.appendChild(input);
+
+        var textDiv = document.createElement("div");
+        textDiv.classList = "col descriptionText"
 		var text = document.createTextNode(colors[j]["text"]);
-		input.id = o + (indexes.length == 0 ? j : indexes[j]);
-		input.className = "jscolor";
-		input.value = colors[j]["color"];
+        textDiv.appendChild(text);
+
+        row.appendChild(inputDiv);
+        row.appendChild(textDiv);
+
+        area.appendChild(row);
+
 		area.appendChild(input);
 		area.appendChild(text);
 		area.appendChild(document.createElement("br"));
@@ -86,29 +103,34 @@ function create_button(i, o, indexes, sect){
 }
 
 // Create all buttons, on load
-function create_buttons(){
-	for (var i=0;i<buttons.length;i++){
+function create_buttons() {
+    for (var i = 0; i < buttons.length; i++) {
 		create_button(i, buttons[i]["name"], buttons[i]["indexes"], buttons[i]["section"]);
 	}
 	log("Info: Successfully loaded NPC.");
 }
 
 // Update value of variables with selected colors from colorpickers
-function update_buttons(){
-	for (var o in objects){
-		for (var j=0;j<objects[o].length;j++){
+function update_buttons() {
+    for (var o in objects) {
+        for (var j = 0; j < objects[o].length; j++) {
 			//if (document.getElementById(o+j) === null) { console.log(o+j); }
-			objects[o][j]["color"] = document.getElementById(o+j).value;
+            objects[o][j]["color"] = document.getElementById(o + j).value;
+        }
+    }
+}
 		}
 	}
 }
 
 // Changes the current tab view
-function tab(item, type){
+function tab(item, type) {
 	var i, tabs, obj;
 	obj = document.getElementById(item);
 	tabs = document.getElementsByClassName(type == "tab" ? "page" : "section");
-	for (i=0;i<tabs.length;i++){ tabs[i].style.display = "none"; }
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
 	obj.style.display = "block";
 }
 
@@ -119,49 +141,50 @@ function tab(item, type){
  */
 
 // Logging method
-function log(string){
+function log(string) {
 	var fileDisplayArea = document.getElementById('fileDisplayArea');
 	fileDisplayArea.innerHTML += (moment().format("HH:mm:ss") + " - " + string + "\n");
 }
 
 // Create blob from string of bytes
-function blobify(text){
+function blobify(text) {
 	var bytes = new Uint8Array(text.match(/.{2}/g).map(e => parseInt(e, 16)));
-	return new Blob([bytes], {type: "application/octet-stream"});
+    return new Blob([bytes], { type: "application/octet-stream" });
 }
 
 // Create string of bytes from blob
-function deblobify(blob){
+function deblobify(blob) {
 	var binary = "";
 	var bytes = new Uint8Array(blob);
-	for (var i=0;i<bytes.byteLength;i++){
+    for (var i = 0; i < bytes.byteLength; i++) {
 		binary += bytes[i].toString(16).padStart(2, '0').toUpperCase();
 	}
 	return binary;
 }
 
 // Creates ArrayBuffer from string
-function bufferize(text){
+function bufferize(text) {
 	return new Uint8Array(text.match(/.{2}/g).map(e => parseInt(e, 16))).buffer;
 }
 
 // Creates a matrix from an array
-function matrixize(array, size){
+function matrixize(array, size) {
 	var matrix = [];
-	for (var i=0;i<array.length;i+=size) matrix.push(array.slice(i,i+size));
+    for (var i = 0; i < array.length; i += size) matrix.push(array.slice(i, i + size));
 	return matrix;
 }
 
 // Listener for file uploading
-function list(){
+function list() {
 	var end_index = this.f.name.length - 4;
-	files[this.f.name.substring(0,end_index)] = deblobify(this.result);
-	files_loaded[this.f.name.substring(0,end_index)] = true;
+    files[this.f.name.substring(0, end_index)] = deblobify(this.result);
+    files_loaded[this.f.name.substring(0, end_index)] = true;
 }
-function check_palette(){
+
+function check_palette() {
 	var done = true;
 	var objs = Object.keys(objects);
-	for (var i=0;i<objs.length;i++){
+    for (var i = 0; i < objs.length; i++) {
 		if (!files.hasOwnProperty(objs[i]) ||
 				!files_loaded.hasOwnProperty(objs[i]) ||
 				!files_loaded[objs[i]]) {
@@ -185,20 +208,20 @@ window.onload = function() {
 		// Retrieve selected files
 		var aux = fileInput.files;
 		var files_raw = [];
-		for (var i=0;i<aux.length;i++){ files_raw[i] = aux[i]; }
+        for (var i = 0; i < aux.length; i++) { files_raw[i] = aux[i]; }
 		// Remove extraneous files
 		var objs = Object.keys(objects);
-		for (var i=0;i<files_raw.length;i++){
+        for (var i = 0; i < files_raw.length; i++) {
 			var end_index = files_raw[i].name.length - 4;
-			var filename = files_raw[i].name.substring(0,end_index);
+            var filename = files_raw[i].name.substring(0, end_index);
 			if (!objs.includes(filename)) { files_raw.splice(i, 1); }
 		}
 		// Check for existence of all palette files
 		var filenames = [];
-		for (var i=0;i<files_raw.length;i++){ filenames.push(files_raw[i].name); }
+        for (var i = 0; i < files_raw.length; i++) { filenames.push(files_raw[i].name); }
 		var inexistant = [];
 		var inex_count = 0;
-		for (var i=0;i<objs.length;i++){
+        for (var i = 0; i < objs.length; i++) {
 			if (!filenames.includes(objs[i] + ".tga")) {
 				inexistant.push(objs[i] + ".tga");
 				inex_count += 1;
@@ -211,7 +234,7 @@ window.onload = function() {
 			log("Error loading palette: Missing " + inex_count.toString() + " files.");
 		} else {
 			// Read the files
-			for (var i=0;i<files_raw.length;i++){
+            for (var i = 0; i < files_raw.length; i++) {
 				var file = files_raw[i];
 				var reader = new FileReader();
 				reader.f = file;
@@ -233,13 +256,13 @@ window.onload = function() {
 
 // Creates a tga file as a string of raw bytes in hex
 // (Read the TGA specs to understand this function)
-function create_file(name){
+function create_file(name) {
 	var i, j, n, w, f;
 	n = objects[name].length;
 	w = (64 * n).toString(16).padStart(4, '0').match(/.{2}/g).reverse().join('');
 	f = "00000A000000000000000000" + w + "40001800";
-	for (i=0;i<64;i++){
-		for (j=0;j<n;j++){
+    for (i = 0; i < 64; i++) {
+        for (j = 0; j < n; j++) {
 			f += "BF" + objects[name][j]["color"].match(/.{2}/g).reverse().join("");
 		}
 	}
@@ -247,12 +270,12 @@ function create_file(name){
 }
 
 // Generate all palette files, zip them and serve them
-function create_palette(){
+function create_palette() {
 	update_buttons();
 	var objs = Object.keys(objects).map(o => blobify(create_file(o)));
 	var zip = new JSZip();
-	for (var i=0;i<objs.length;i++){ zip.file(Object.keys(objects)[i] + ".tga", objs[i]); }
-	zip.generateAsync({type:"blob"}).then(
+    for (var i = 0; i < objs.length; i++) { zip.file(Object.keys(objects)[i] + ".tga", objs[i]); }
+    zip.generateAsync({ type: "blob" }).then(
 		function(content) {
   		saveAs(content, "palette.zip");
 		}
@@ -267,19 +290,19 @@ function create_palette(){
  */
 
 // Check that the TGA is valid for our purpose
-function check_file(filename){
+function check_file(filename) {
 	// Load file
 	var errorMessage = "ERRORS with file " + filename + ":\n";
 	var errorHappened = false;
 	var file = files[filename].match(/.{2}/g);
-	var header = file.slice(0,18);
+    var header = file.slice(0, 18);
 	// Extract header fields
 	var id_length = parseInt(header[0], 16);
 	var colormap_type = parseInt(header[1], 16);
 	var image_type = parseInt(header[2], 16);
-	var colormap_spec = header.slice(3,8);
-	var width = parseInt(header.slice(12,14).reverse().join(""), 16);
-	var height = parseInt(header.slice(14,16).reverse().join(""), 16);
+    var colormap_spec = header.slice(3, 8);
+    var width = parseInt(header.slice(12, 14).reverse().join(""), 16);
+    var height = parseInt(header.slice(14, 16).reverse().join(""), 16);
 	var pixel_depth = parseInt(header[16], 16);
 	var colors = Math.round(width / 64);
 	// The following used to be check from when I wasn't using a TGA library
@@ -329,9 +352,9 @@ function check_file(filename){
 	*/
 	if (colors != objects[filename].length) {
 		errorHappened = true;
-		errorMessage += ("* The image doesn't have the right amount of colors (has "
-								 + colors.toString() + ", should have "
-								 + objects[filename].length.toString() + ").\n");
+        errorMessage += ("* The image doesn't have the right amount of colors (has " +
+            colors.toString() + ", should have " +
+            objects[filename].length.toString() + ").\n");
 	}
 	if (height != 64) {
 		errorMessage += "* Warning: The image height should be 64.\n"
@@ -344,7 +367,7 @@ function check_file(filename){
 }
 
 // Parses a tga file from a string of raw bytes in hex
-function parse_file(filename){
+function parse_file(filename) {
 	var result = check_file(filename);
 	if (result === true) {
 		// Change colorboxes (pixels (31,32) + (0,64*i))
@@ -357,14 +380,14 @@ function parse_file(filename){
 			var n = width / 64;
 			var image = tga.getImageData();
 			var pixels = matrixize(matrixize(image.data, 4), width);
-			for (var i=0;i<n;i++){
-				var rgb = deblobify(pixels[31][32+64*i]).substring(0,6);
+            for (var i = 0; i < n; i++) {
+                var rgb = deblobify(pixels[31][32 + 64 * i]).substring(0, 6);
 				var input = document.getElementById(filename + i.toString());
 				input.value = rgb;
 				input.jscolor.fromString(rgb);
 			}
 			return true;
-		} catch(e) {
+        } catch (e) {
 			return e.message;
 		}
 	} else {
@@ -373,13 +396,13 @@ function parse_file(filename){
 }
 
 // Parses all palette files
-function parse_palette(){
+function parse_palette() {
 	var objs = Object.keys(objects);
 	var fileDisplayArea = document.getElementById('fileDisplayArea');
 	var errorMessage = "ERROR loading palette:\n\n";
 	var errors = false;
 	var invalid = [];
-	for (var i=0;i<objs.length;i++){
+    for (var i = 0; i < objs.length; i++) {
 		var result = parse_file(objs[i]);
 		if (!(result === true)) {
 			errorMessage += (result + "\n");
@@ -399,7 +422,7 @@ function parse_palette(){
 	}
 }
 
-function test(){
+function test() {
 	var file = files["background"];
 	var tga = new TGA();
 	var buffer = bufferize(file);
@@ -409,8 +432,8 @@ function test(){
 	var image = tga.getImageData();
 	var pixels = matrixize(matrixize(image.data, 4), width);
 	var colors = new Array(n);
-	for (var i=0;i<n;i++){
-		var rgb = deblobify(pixels[31][32+64*i]).substring(0,6);
+    for (var i = 0; i < n; i++) {
+        var rgb = deblobify(pixels[31][32 + 64 * i]).substring(0, 6);
 		colors[i] = rgb;
 		log(rgb);
 	}
